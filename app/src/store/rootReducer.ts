565a9fe9e1fc60a901c3fc200c4cf63f083026f3
Jack Woodmansey <jwoodmansey@live.co.uk>
2020-09-26 14:33:05 +0100

    env

--- app/src/store/rootReducer.ts
+++ app/src/store/rootReducer.ts
@@ -4,9 +4,21 @@ import { firestoreReducer } from "redux-firestore";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-const firebaseConfig = process.env.firebaseConfig
-  ? JSON.parse(process.env.firebaseConfig)
-  : {};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// require("dotenv").config({ debug: true });
+
+//   console.log("PROCESS", process.env.FIREBASE_CONFIG);
+const firebaseConfig = process.env.FIREBASE_CONFIG
+  ? JSON.parse(process.env.FIREBASE_CONFIG)
+  : {
+      apiKey: "AIzaSyC2HUqp0j9kaRRTnTa6kV8HtKUlo_yBD1c",
+      authDomain: "working-wall.firebaseapp.com",
+      databaseURL: "https://working-wall.firebaseio.com",
+      projectId: "working-wall",
+      storageBucket: "working-wall.appspot.com",
+      messagingSenderId: "918611508376",
+      appId: "1:918611508376:web:1ef0ba4545baab3b21e4ed",
+    };
 
 // Initialize firebase instance
 firebase.initializeApp(firebaseConfig);
